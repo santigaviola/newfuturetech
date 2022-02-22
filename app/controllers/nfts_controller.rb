bafb989c67d322506425c78e5babba5c82d0b61b
Santiago Gaviola <santijgaviola@gmail.com>
2022-02-22 16:15:24 -0300

    worked on transactions controller

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,47 +1,48 @@
 class NftsController < ApplicationController
   before_action :set_nft, only:[:show, :update, :edit, :destroy]
-  
-    def index
-      @nfts = Nft.all
-    end
-  
-    def show
-    end
 
-    def new
-      @nft = Nft.new
+  def index
+    @nfts = Nft.all
+  end
+
+  def show
+  end
+
+  def new
+    @nft = Nft.new
+  end
+
+  def create
+    @nft = Nft.new(nft_params)
+    @nft.user = current_user
+    if @nft.save
+      redirect_to nft_path(@nft)
+    else
+      render "new"
     end
-  
-    def create
-      @nft = Nft.new(nft_params)
-      @nft.user = current_user
-      if @nft.save
+  end
+
+  def update
+    @nft.update(nft_params)
     redirect_to nft_path(@nft)
-      else
-        render "new"
-      end
-    end
-  
-    def update
-      @nft.update(nft_params)
-      redirect_to nft_path(@nft)
-    end
-  
-    def edit
-    end
-  
-    def destroy
-      @nft.destroy
-      redirect_to nft_path(@nft)
-    end
+  end
 
-    private
-    
-    def set_nft
-      @nft = nft.find(params[:id])
-    end
+  def edit
+  end
 
-    def nft_params
-      params.require(:nft).permit(:name, :description, :image_url, :published, :price)
-    end
+  def destroy
+    @nft.destroy
+    redirect_to nft_path(@nft)
+  end
+
+  private
+
+  def set_nft
+    @nft = Nft.find(params[:id])
+  end
+
+  def nft_params
+    params.require(:nft).permit(:name, :description, :image_url, :published, :price)
+  end
+
 end
