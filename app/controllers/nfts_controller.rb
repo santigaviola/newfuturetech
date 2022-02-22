889414332ea54816af86a77c37dd74a25be5eb30
KingAlex1997 <alex97l@hotmail.com>
2022-02-22 12:04:41 -0300

    cambios en los metodos de controlador de nfts

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,2 +1,48 @@
 class NftsController < ApplicationController
+  before_action :set_nft, only:[:show, :update, :edit, :destroy]
+
+  
+    def index
+      @nfts = Nft.all
+    end
+  
+    def show
+    end
+
+    def new
+      @nft = nft.new
+    end
+  
+    def create
+      @nft = nft.new(nft_params)
+      @nft.user = current_user
+      if @nft.save
+    redirect_to nft_path(@nft)
+      else
+        render "new"
+      end
+    end
+  
+    def update
+      @nft.update(nft_params)
+      redirect_to nft_path(@nft)
+    end
+  
+    def edit
+    end
+  
+    def destroy
+      @nft.destroy
+      redirect_to nft_path(@nft)
+    end
+
+    private
+    
+    def set_nft
+      @nft = nft.find(params[:id])
+    end
+
+    def nft_params
+      params.require(:nft).permit(:name, :description, :image_url, :published, :price)
+    end
 end
