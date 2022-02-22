ff4662276e9df0d531780cc35b6e01dddf40fbd8
Santiago Gaviola <santijgaviola@gmail.com>
2022-02-22 17:19:01 -0300

    corregi el estilo de nfts controller

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,37 +1,47 @@
 class NftsController < ApplicationController
-  before_action :set_nft, only:[:show, :update, :edit, :destroy]
-    def index
-      @nfts = Nft.all
-    end
-    def show
-    end
-    def new
-      @nft = Nft.new
-    end
-    def create
-      @nft = Nft.new(nft_params)
-      @nft.user = current_user
-      if @nft.save
-        redirect_to nfts_path
-      else
-        render "new"
-      end
-    end
-    def update
-      @nft.update(nft_params)
-      redirect_to nft_path(@nft)
-    end
-    def edit
-    end
-    def destroy
-      @nft.destroy
+  before_action :set_nft, only: %i[show update edit destroy]
+
+  def index
+    @nfts = Nft.all
+  end
+
+  def show
+  end
+
+  def new
+    @nft = Nft.new
+  end
+
+  def create
+    @nft = Nft.new(nft_params)
+    @nft.user = current_user
+    if @nft.save
       redirect_to nfts_path
+    else
+      render "new"
     end
-    private
-    def set_nft
-      @nft = Nft.find(params[:id])
-    end
-    def nft_params
-      params.require(:nft).permit(:name, :description, :image_url, :published, :price)
-    end
+  end
+
+  def update
+    @nft.update(nft_params)
+    redirect_to nft_path(@nft)
+  end
+
+  def edit
+  end
+
+  def destroy
+    @nft.destroy
+    redirect_to nfts_path
+  end
+
+  private
+
+  def set_nft
+    @nft = Nft.find(params[:id])
+  end
+
+  def nft_params
+    params.require(:nft).permit(:name, :description, :image_url, :published, :price)
+  end
 end
