38532ed0c3c0dba0a28f320e9a2f3f9aab49d182
diego ripari <diego_ripari@hotmail.com>
2022-02-22 18:49:22 -0300

    policies para loguiar

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,37 +1,54 @@
 class NftsController < ApplicationController
   before_action :set_nft, only:[:show, :update, :edit, :destroy]
-    def index
-      @nfts = Nft.all
-    end
-    def show
-    end
-    def new
-      @nft = Nft.new
-    end
-    def create
-      @nft = Nft.new(nft_params)
-      @nft.user = current_user
-      if @nft.save
-        redirect_to nfts_path
-      else
-        render "new"
-      end
-    end
-    def update
-      @nft.update(nft_params)
-      redirect_to nft_path(@nft)
-    end
-    def edit
-    end
-    def destroy
-      @nft.destroy
+
+
+  def index
+    @nfts = policy_scope(Nft).order(created_at: :desc)
+  end
+
+  def show
+    authorize @nft
+  end
+
+  def new
+    @nft = Nft.new
+    authorize @nft
+  end
+
+  def create
+    @nft = Nft.new(nft_params)
+    authorize @nft
+    @nft.user = current_user
+    if @nft.save
       redirect_to nfts_path
+    else
+      render "new"
     end
-    private
-    def set_nft
-      @nft = Nft.find(params[:id])
-    end
-    def nft_params
-      params.require(:nft).permit(:name, :description, :image_url, :published, :price)
-    end
+  end
+
+  def update
+    authorize @nft
+    @nft.update(nft_params)
+    redirect_to nft_path(@nft)
+  end
+
+  def edit
+    authorize @nft
+  end
+
+  def destroy
+    authorize @nft
+    @nft.destroy
+    redirect_to nfts_path
+  end
+
+  private
+
+  def set_nft
+    @nft = Nft.find(params[:id])
+  end
+
+  def nft_params
+    params.require(:nft).permit(:name, :description, :image_url, :published, :price)
+  end
 end
