94cc4f50253c2e87014f8b38ed0833f84a77b2d3
KingAlex1997 <alex97l@hotmail.com>
2022-02-23 16:51:53 -0300

    cambios de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Nft.destroy_all
+
+crealo = User.new(email: "crealo@hotmail.com", password: "123456", username: "Crealo")
+3.times do
+      nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, image_url: "http://source.unsplash.com/1600x900/?nft",published: true, price: rand(1...1000))
+      nft.user = crealo
+      nft.save
+end
+
+  10.times do
+    user = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username)
+    user.save
+    3.times do
+      nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, image_url: "http://source.unsplash.com/1600x900/?nft",published: true, price: rand(1...1000))
+      nft.user = user
+      nft.save
+    end
+  end
+
+
