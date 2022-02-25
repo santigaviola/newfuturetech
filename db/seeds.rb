dc41d7702f0c40485c840b94ea1fe77833280968
Santiago Gaviola <santijgaviola@gmail.com>
2022-02-25 12:21:51 -0300

    deleted photo connection to home.js

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,22 @@ User.destroy_all
 Nft.destroy_all
 
 crealo = User.new(email: "crealo@hotmail.com", password: "123456", username: "Crealo")
+random_number = rand(1...1000)
+
 3.times do
-      nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, image_url: "http://source.unsplash.com/1600x900/?nft",published: true, price: rand(1...1000))
-      nft.user = crealo
-      nft.save
+  nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, published: true, price: rand(1...1000))
+  nft.photo.key = "http://source.unsplash.com/1600x900/?#{random_number}"
+  nft.user = crealo
+  nft.save
 end
 
-  10.times do
-    user = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username)
-    user.save
-    3.times do
-      nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, image_url: "http://source.unsplash.com/1600x900/?nft",published: true, price: rand(1...1000))
-      nft.user = user
-      nft.save
-    end
+4.times do
+  user = User.new(email: Faker::Internet.email, password: Faker::Internet.password, username: Faker::Internet.username)
+  user.save
+  3.times do
+    nft = Nft.new(name: Faker::FunnyName.name, description: Faker::Lorem.paragraph, published: true, price: rand(1...1000))
+    nft.photo.key = "http://source.unsplash.com/1600x900/?#{random_number}"
+    nft.user = user
+    nft.save
   end
-
-
+end
